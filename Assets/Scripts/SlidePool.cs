77be9669baccab9fb4d6949a39955f295c2a65bd
Aric Huang <arichuang@gmail.com>
2018-07-21 14:35:50 -0700

    generate level sections

--- Assets/Scripts/SlidePool.cs
+++ Assets/Scripts/SlidePool.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class SlidePool : MonoBehaviour {
 
-	private GameObject[] slides;
+	// private GameObject[] slides;
+	private GameObject[][] levelSections;
 	public int slidePoolSize = 6;
+
+	public int levelSectionBufferSize = 3;
 	public GameObject slidePrefab;
-	public float spawnRate = 0.3777f;
+	public float spawnRate = 2f;
 	public float ySlideMin = -5f;
 	public float ySlideMax = 5f;
 	public float xSlideMin = 0f;
 	public float xSlideMax = 10f;
 
 	private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
-	private int currentSlide = 0;
+	private int currentLevelSection = 0;
 	private Quaternion spawnRotation = Quaternion.Euler(0,0,270);
 
 	private float timeSinceLastSpawned;
@@ -22,9 +25,33 @@ public class SlidePool : MonoBehaviour {
 	//Use this for initialization
 	void Start () {
 		timeSinceLastSpawned = spawnRate;
-		slides = new GameObject[slidePoolSize];
+		levelSections = new GameObject[levelSectionBufferSize][];
+		// Instantiate the level sections.
+		for (int i = 0; i < levelSectionBufferSize; i++) {
+			var slides = new GameObject[slidePoolSize];
+			for (int j = 0; j < slidePoolSize; j++) {
+				slides[j] = (GameObject) Instantiate(slidePrefab, objectPoolPosition, spawnRotation);
+			}
+			levelSections[i] = slides;
+		}
+	}
+
+	void FormatLevelSection(GameObject[] levelSection) {
+		var positions = new Vector2[]{
+			new Vector2(3f, -1f),
+            new Vector2(10f, -7f),
+            new Vector2(9f, 3f),
+            new Vector2(13f, 1f),
+            new Vector2(16f, -5f),
+            new Vector2(21f, -1f),
+		};
+		// move it offscreen
+		var xOffset = 16f + Random.Range(-4f, 4f);
+		var yOffset = Random.Range(-4f, 4f);
+
 		for (int i = 0; i < slidePoolSize; i++) {
-			slides[i] = (GameObject) Instantiate(slidePrefab, objectPoolPosition, spawnRotation);
+			var position = positions[i];
+			levelSection[i].transform.position = new Vector2(position.x + xOffset, position.y + yOffset);
 		}
 	}
 
@@ -32,12 +59,18 @@ public class SlidePool : MonoBehaviour {
 	void Update () {
 		timeSinceLastSpawned += Time.deltaTime;
 
+
 		if (GameController.instance.gameOver == false && timeSinceLastSpawned >= spawnRate) {
 			timeSinceLastSpawned = 0;
-			float spawnYPos = Random.Range(ySlideMin, ySlideMax);
-			float spawnXPos = Random.Range(xSlideMin, xSlideMax);
-			slides[currentSlide].transform.position = new Vector2(spawnXPos, spawnYPos);
-			currentSlide = (currentSlide + 1) % slidePoolSize;
+
+            Debug.Log(levelSections[currentLevelSection].Length);
+			FormatLevelSection(levelSections[currentLevelSection]);
+
+			// float spawnYPos = Random.Range(ySlideMin, ySlideMax);
+			// float spawnXPos = Random.Range(xSlideMin, xSlideMax);
+			// slides[currentSlide].transform.position = new Vector2(spawnXPos, spawnYPos);
+			currentLevelSection = (currentLevelSection + 1) % levelSectionBufferSize;
+
 		}
 	}
 }
